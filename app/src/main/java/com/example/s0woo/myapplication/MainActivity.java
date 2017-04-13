65f315a55dbb967f2b6db9e8d337ed4b64b03779
SimYoungwoo <youngwoo0411@naver.com>
2017-04-13 23:55:02 +0900

    20170413

--- app/src/main/java/com/example/s0woo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/s0woo/myapplication/MainActivity.java
@@ -5,14 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-
+import com.skp.Tmap.TMapData;
+import com.skp.Tmap.TMapPOIItem;
 import com.skp.Tmap.TMapTapi;
+import com.skp.Tmap.TMapData.FindAllPOIListenerCallback;
+import com.skp.Tmap.TMapView;
 
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.String;
+import android.util.Log;
+import java.util.logging.*;
+
+import java.util.ArrayList;
 
 
+//import com.skplanetx.tmapopenmapapi.LogManager;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     public EditText editStart;
     public ImageButton btnStart;
@@ -23,10 +38,21 @@ public class MainActivity extends AppCompatActivity {
     public EditText editStop1;
     public ImageButton btnStop1;
 
+    public static String mApiKey = "";
+    private TMapView mMapView = null;
+
+    private void configureMapView() {
+        mMapView.setSKPMapApiKey(mApiKey);
+        //song
+//		mMapView.setSKPMapBizappId(mBizAppID);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         editStart = (EditText)findViewById(R.id.startEdit);
         btnStart = (ImageButton)findViewById(R.id.startBtn);
@@ -41,13 +67,40 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String str = editStart.getText().toString();
+                if (str == null || str.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "입력값이 필요합니다.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //System.out.println("입력성공");
+                    Toast.makeText(getApplicationContext(), "입력성공." + str, Toast.LENGTH_SHORT).show();
 
+                    TMapData tmapdata = new TMapData();
+
+                    tmapdata.findAllPOI(str, new FindAllPOIListenerCallback() {
+                        @Override
+                        public void onFindAllPOI(ArrayList<TMapPOIItem> poiItem) {
+                            for (int i = 0; i < poiItem.size(); i++) {
+                                TMapPOIItem  item = poiItem.get(i);
+
+                                //Log.d(TAG, "POI Name: " + item.getPOIName().toString() + ", " +
+                                //        "Address: " + item.getPOIAddress().replace("null", "")  + ", " +
+                                //        "Point: " + item.getPOIPoint().toString());
+                            }
+                        }
+                    });
+
+
+                }
             }
         });
+
 
         btnFinish.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String str = editFinish.getText().toString();
+                if (str == null || str.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "입력값이 필요합니다.", Toast.LENGTH_LONG).show();
+                }
 
             }
         });
@@ -55,6 +108,9 @@ public class MainActivity extends AppCompatActivity {
         btnStop1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String str = editStop1.getText().toString();
+                if (str == null || str.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "입력값이 필요합니다.", Toast.LENGTH_LONG).show();
+                }
 
             }
         });
